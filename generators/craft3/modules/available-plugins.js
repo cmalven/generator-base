bda2fb6a37986c4a74c8aad0bc511549b8919b6f
Chris Malven <cmalven@chrismalven.com>
2018-12-26 17:21:20 -0600

    Update Craft plugin defaults.

--- generators/craft3/modules/available-plugins.js
+++ generators/craft3/modules/available-plugins.js
@@ -47,11 +47,36 @@ module.exports = [
   {
     name: 'Redactor',
     src: 'craftcms/redactor',
+    checked: true
+  },
+  {
+    name: 'Environment Label',
+    src: 'topshelfcraft/environment-label',
+    checked: true
+  },
+  {
+    name: 'Twig Profiler',
+    src: 'nystudio107/craft-twigprofiler',
+    checked: true
+  },
+  {
+    name: 'Placeholder Pixel',
+    src: 'tibemolde/placeholder-pixel',
+    checked: true
+  },
+  {
+    name: 'Bits',
+    src: 'malven/craft-bits',
     checked: false
   },
   {
-    name: 'Simple Text',
-    src: 'craftcms/simple-text',
+    name: 'Supertable',
+    src: 'verbb/super-table',
+    checked: false
+  },
+  {
+    name: 'Craft Sentry',
+    src: 'lukeyouell/craft-sentry',
     checked: false
   },
   {
@@ -60,8 +85,8 @@ module.exports = [
     checked: false
   },
   {
-    name: 'Environment Label',
-    src: 'topshelfcraft/environment-label',
-    checked: true
+    name: 'CP Nav',
+    src: 'verbb/cp-nav',
+    checked: false
   }
 ];
