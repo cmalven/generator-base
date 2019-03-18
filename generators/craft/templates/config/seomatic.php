b1a06bc43f69e66e2d4a9b5f6a1f9429160c0b5b
Chris Malven <cmalven@chrismalven.com>
2019-03-18 14:50:48 -0500

    Add craft environment config setting and let SEOmatic tie into that.

--- generators/craft/templates/config/seomatic.php
+++ generators/craft/templates/config/seomatic.php
@@ -3,8 +3,5 @@ return [
     '*' => [
         // The public-facing name of the plugin
         'pluginName' => 'SEO',
-
-        // The server environment, either `live`, `staging`, or `local`
-        'environment' => getenv('SEO_ENV'),
     ]
 ];
