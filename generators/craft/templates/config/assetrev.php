d92d57d73361ade5b72e250966a64c8e6e037c4f
Chris Malven <cmalven@chrismalven.com>
2019-07-05 11:55:43 -0500

    Remove query string strategy from assetrev caching.

--- generators/craft/templates/config/assetrev.php
+++ generators/craft/templates/config/assetrev.php
@@ -3,12 +3,11 @@ return array(
     '*' => array(
         'strategies' => [
             'manifest' => \club\assetrev\utilities\strategies\ManifestFileStrategy::class,
-            'querystring' => \club\assetrev\utilities\strategies\QueryStringStrategy::class,
             'passthrough' => function ($filename, $config) {
                 return $filename;
             },
         ],
-        'pipeline' => 'manifest|querystring|passthrough',
+        'pipeline' => 'manifest|passthrough',
         'manifestPath' => 'web/dist/rev-manifest.json',
         'assetsBasePath' => 'web/dist/',
         'assetUrlPrefix' => '/dist/'
