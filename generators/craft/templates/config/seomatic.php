2043aede58a66411bc9b5507e65be04c5bb1bee2
cmalven <chris@malven.co>
2020-06-15 20:49:55 -0500

    Don’t announce that site is powered by SEOmatic.

--- generators/craft/templates/config/seomatic.php
+++ generators/craft/templates/config/seomatic.php
@@ -3,5 +3,8 @@ return [
     '*' => [
         // The public-facing name of the plugin
         'pluginName' => 'SEO',
+
+        // Don't announce that the site is powered by SEOmatic
+        'generatorEnabled' => false,
     ]
 ];
