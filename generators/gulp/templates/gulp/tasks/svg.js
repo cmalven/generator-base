cf2d5066575b4e976b1fc5253ec09cb3eb08cf8b
cmalven <chris@malven.co>
2020-08-02 07:47:23 -0500

    Don't namespace ids

--- generators/gulp/templates/gulp/tasks/svg.js
+++ generators/gulp/templates/gulp/tasks/svg.js
@@ -19,7 +19,7 @@ var defaultSVGO = [
   { moveElemsAttrsToGroup: false },
   { moveGroupAttrsToElems: false },
   { removeUselessStrokeAndFill: false },
-  { removeViewBox: false }
+  { removeViewBox: false },
 ];
 
 gulp.task('svg:clean', function() {
@@ -28,74 +28,78 @@ gulp.task('svg:clean', function() {
 
 gulp.task('svg:icon', function() {
   return gulp.src([
-    config.paths.imageSrc + 'svg/icon/**/*.svg'
+    config.paths.imageSrc + 'svg/icon/**/*.svg',
   ])
     .pipe(svgSprite({
       mode: {
         symbol: {
-          sprite: 'icon.symbol.svg'
+          sprite: 'icon.symbol.svg',
         },
-        inline: true
+        inline: true,
       },
       shape: {
         id: {
           generator: function(name) {
             return name.replace('.svg', '');
-          }
+          },
         },
         transform: [
           { svgo: {
             plugins: [
               { removeAttrs: { attrs: ['opacity'] } },
-              { convertColors: { currentColor: true } }
-            ].concat(defaultSVGO)
-          } }
-        ]
-      }
+              { convertColors: { currentColor: true } },
+            ].concat(defaultSVGO),
+          } },
+        ],
+      },
     }))
     .pipe(gulp.dest(config.paths.templateSrc + '_svg/'));
 });
 
 gulp.task('svg:full', function() {
   return gulp.src([
-    config.paths.imageSrc + 'svg/full/**/*.svg'
+    config.paths.imageSrc + 'svg/full/**/*.svg',
   ])
     .pipe(svgSprite({
       mode: {
         symbol: {
-          sprite: 'full.symbol.svg'
+          sprite: 'full.symbol.svg',
         },
-        inline: true
+        inline: true,
       },
       shape: {
         id: {
           generator: function(name) {
             return name.replace('.svg', '');
-          }
+          },
         },
         transform: [
           { svgo: {
-            plugins: [].concat(defaultSVGO)
-          } }
-        ]
-      }
+            plugins: [].concat(defaultSVGO),
+          } },
+        ],
+      },
     }))
     .pipe(gulp.dest(config.paths.templateSrc + '_svg/'));
 });
 
 gulp.task('svg:inline', function() {
   return gulp.src([
-    config.paths.imageSrc + 'svg/inline/**/*.svg'
+    config.paths.imageSrc + 'svg/inline/**/*.svg',
   ])
     .pipe(svgSprite({
       shape: {
         dest: 'inline',
         transform: [
           { svgo: {
-            plugins: [].concat(defaultSVGO)
-          } }
-        ]
-      }
+            plugins: [].concat(defaultSVGO),
+          } },
+        ],
+      },
+      // Comment out if you'd like to avoid possible ID collisions
+      svg: {
+        namespaceIDs: false,
+      },
     }))
     .pipe(gulp.dest(config.paths.templateSrc + '_svg/'));
 });
