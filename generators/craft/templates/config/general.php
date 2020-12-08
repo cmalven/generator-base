dd0caf6361fd0321ec49f75c815347cd796f7510
cmalven <chris@malven.co>
2020-12-08 09:53:30 -0600

    Enable project config by default.

--- generators/craft/templates/config/general.php
+++ generators/craft/templates/config/general.php
@@ -9,72 +9,68 @@
 
 return [
     '*' => [
-      // This appId is used to generate a unique prefix for session cookies and cache locations across our dev environments
-      'appId'                              => getenv('SITE_URL'),
+        // This appId is used to generate a unique prefix for session cookies and cache locations across our dev environments
+        'appId'                              => getenv('SITE_URL'),
 
-      // FUZZY SEARCH
-      'defaultSearchTermOptions' => array(
-          'subLeft' => true,
-          'subRight' => true,
-      ),
+        // FUZZY SEARCH
+        'defaultSearchTermOptions' => array(
+            'subLeft' => true,
+            'subRight' => true,
+        ),
 
-      // ASSETS
-      'imageDriver'                        => getenv('IMAGE_DRIVER'),
-      'defaultImageQuality'                => getenv('DEFAULT_IMAGE_QUALITY'),
-      'extraAllowedFileExtensions'         => getenv('EXTRA_ALLOWED_FILE_EXTENSIONS'),
-      'maxUploadFileSize'                  => '4M',
-      'extraFileKinds' => [
-          'svg' => [
-              'label' => 'SVG',
-              'extensions' => ['svg'],
-          ],
-          'jpg' => [
-              'label' => 'JPEG',
-              'extensions' => ['jpg', 'jpeg'],
-          ],
-          'png' => [
-              'label' => 'PNG',
-              'extensions' => ['png'],
-          ],
-          'mp4' => [
-              'label' => 'MP4',
-              'extensions' => ['mp4'],
-          ],
-      ],
+        // ASSETS
+        'imageDriver'                        => getenv('IMAGE_DRIVER'),
+        'defaultImageQuality'                => getenv('DEFAULT_IMAGE_QUALITY'),
+        'extraAllowedFileExtensions'         => getenv('EXTRA_ALLOWED_FILE_EXTENSIONS'),
+        'maxUploadFileSize'                  => '4M',
+        'extraFileKinds' => [
+            'svg' => [
+                'label' => 'SVG',
+                'extensions' => ['svg'],
+            ],
+            'jpg' => [
+                'label' => 'JPEG',
+                'extensions' => ['jpg', 'jpeg'],
+            ],
+            'png' => [
+                'label' => 'PNG',
+                'extensions' => ['png'],
+            ],
+            'mp4' => [
+                'label' => 'MP4',
+                'extensions' => ['mp4'],
+            ],
+        ],
 
-      // MISC
-      'devMode'                            => filter_var(getenv('DEV_MODE'), FILTER_VALIDATE_BOOLEAN),
-      'phpMaxMemoryLimit'                  => getenv('PHP_MAX_MEMORY_LIMIT'),
-      'overridePhpSessionLocation'         => getenv('OVERRIDE_PHP_SESSION_LOCATION'),
-      'allowUpdates'                       => false,
+        // MISC
+        'devMode'                            => filter_var(getenv('DEV_MODE'), FILTER_VALIDATE_BOOLEAN),
+        'phpMaxMemoryLimit'                  => getenv('PHP_MAX_MEMORY_LIMIT'),
+        'overridePhpSessionLocation'         => getenv('OVERRIDE_PHP_SESSION_LOCATION'),
+        'allowUpdates'                       => false,
+        'allowAdminChanges'                  => false,
 
-      // URLS
-      'omitScriptNameInUrls'               => true,
-      'siteUrl'                            => getenv('SITE_URL'),
-      'cpTrigger'                          => getenv('CP_TRIGGER'),
+        // URLS
+        'omitScriptNameInUrls'               => true,
+        'siteUrl'                            => getenv('SITE_URL'),
+        'cpTrigger'                          => getenv('CP_TRIGGER'),
 
-      // CACHING
-      'enableTemplateCaching'               => filter_var(getenv('ENABLE_TEMPLATE_CACHING'), FILTER_VALIDATE_BOOLEAN),
-      'maxCachedCloudImageSize'             => 0,
+        // CACHING
+        'enableTemplateCaching'              => filter_var(getenv('ENABLE_TEMPLATE_CACHING'), FILTER_VALIDATE_BOOLEAN),
+        'maxCachedCloudImageSize'            => 0,
 
-      // SECURITY
-      'securityKey'                         => getenv('SECURITY_KEY'),
-      'cooldownDuration'                    => 'PT5M',
-      'invalidLoginWindowDuration'          => 'PT1H',
-      'sendPoweredByHeader'                 => false,
+        // SECURITY
+        'securityKey'                        => getenv('SECURITY_KEY'),
+        'cooldownDuration'                   => 'PT5M',
+        'invalidLoginWindowDuration'         => 'PT1H',
+        'sendPoweredByHeader'                => false,
     ],
 
     'dev' => [
-        'allowUpdates'                      => true,
+        'allowUpdates'                       => true,
+        'allowAdminChanges'                  => true,
     ],
 
-    'staging' => [
-        // Uncomment below if using project config
-        // 'allowAdminChanges'                 => false,
-    ],
+    'staging' => [],
 
-    'production' => [
-        // Uncomment below if using project config
-        // 'allowAdminChanges'                 => false,
-    ],
+    'production' => [],
 ];
