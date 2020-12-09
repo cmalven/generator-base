d442fc5c65ceea4410976bd56ed378ea7097e3db
cmalven <chris@malven.co>
2020-12-09 14:39:27 -0600

    Modernize code.

--- generators/files/util/getClassListFromMarkup.js
+++ generators/files/util/getClassListFromMarkup.js
@@ -1,18 +1,18 @@
 module.exports = (markup) => {
   // Get all unique css classes in html into dict
-  var classRegexp = /class=['"](.*?)['"]/g;
-  var dict = [];
-  var m;
+  const classRegexp = /class=['"](.*?)['"]/g;
+  let dict = [];
+  let m;
   while ((m = classRegexp.exec(markup))) {
-    var classes = m[1].replace(/\s+/g, ' ').trim();
+    let classes = m[1].replace(/\s+/g, ' ').trim();
     classes.split(' ').forEach(function(item) {
       dict[item] = true;
     });
   }
 
   // convert dict to arr
-  var arr = [];
-  for (var key in dict) arr.push(key);
+  let arr = [];
+  for (let key in dict) arr.push(key);
 
   return arr;
 };
