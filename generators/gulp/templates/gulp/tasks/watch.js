ac8b1623de8f761e9882518b0c94cf46166d9220
cmalven <chris@malven.co>
2021-02-08 10:40:53 -0600

    Scripts task should watch for changes to .glsl files.

--- generators/gulp/templates/gulp/tasks/watch.js
+++ generators/gulp/templates/gulp/tasks/watch.js
@@ -12,7 +12,7 @@ Runs tasks when files change
 
 module.exports = gulp.task('watch', function() {
   gulp.watch([config.paths.styleSrc + '**/*.scss'], gulp.series('styles', 'styles:lint', 'rev:clear'));
-  gulp.watch([config.paths.scriptSrc + '**/*.js'], gulp.series(gulp.parallel('scripts:bundle', 'scripts:copy'), 'rev:clear'));<% if (useTwig) { %>
+  gulp.watch([config.paths.scriptSrc + '**/*.js', config.paths.scriptSrc + '**/*.glsl'], gulp.series(gulp.parallel('scripts:bundle', 'scripts:copy'), 'rev:clear'));<% if (useTwig) { %>
   gulp.watch([config.paths.templateSrc + '**/*.twig'], gulp.series('twig'));<% } else { %>
   gulp.watch([config.paths.templateSrc + '**/*.html', config.paths.templateSrc + '**/*.php', config.paths.templateSrc + '**/*.twig'], gulp.series('templates'));
   <% } %>
