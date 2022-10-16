7c174cef4414f60892b24bfab20eb2af5a379290
cmalven <chris@malven.co>
2022-10-16 13:16:57 -0500

    Add default video twig partial.

---
+++ generators/scripts/templates/modules/Video.ts
@@ -0,0 +1,15 @@
+import { Modu, ModuOptions } from '@malven/modu';
+
+/**
+ * Handles behavior of ambient videos.
+ */
+
+export default class extends Modu {
+  constructor(m: ModuOptions) {
+    super(m);
+  }
+
+  init = () => {
+    // DEV: Any future logic goes here
+  };
+}
